f76ff4a0aec4fea4f91c8960779dac5fda75f475
Ronald Zulbaran <35377538+ronaldzulbaran@users.noreply.github.com>
2022-09-15 11:57:36 -0500

    Create ejercicio-2.py

---
+++ python/ejercicio-2.py
@@ -0,0 +1,8 @@
+$ python3
+Python 3.10.4 (main, Jun 29 2022, 12:14:53) [GCC 11.2.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> saludo = "Hola mundo!"
+>>> saludo = "Hola mundo modificado!"
+>>> print(saludo)
+Hola mundo modificado!
+>>> 
